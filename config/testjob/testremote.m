4ce7982587d9356ed34ba8d388f4acb15314e63a
Chris Miller <cbmiller2610@live.com>
2014-03-22 12:04:36 -0400

    Cleaned up
    Cleaned up the old job-specific folders from friday.  The folders
    currently are created during the config process but for some reason it
    is not creating the job-specific environment file, so running
    "cleanupJob" does not work due to not finding that file.  Likely due to
    the process not finishing fully due to errors.

--- config/testjob/testremote.m
+++
@@ -1,9 +0,0 @@
-
-intelCluster = parcluster('genericNonSharedOakleyIntel');
-
-job_intel = batch(intelCluster, @colsum, 1, {}, 'CurrentFolder', '/nfs/14/samsi', ...
-    'matlabpool', 11, 'CaptureDiary', true);
-
-disp('job submitted');
-
-wait(job_intel)
